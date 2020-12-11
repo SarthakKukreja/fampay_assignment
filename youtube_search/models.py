ef4af0c7725ffa8c96d889009523e4ff3e51d149
Sarthak Kukreja <kukreja_sarthak@yahoo.com>
2020-12-11 07:25:49 +0530

    add-db-model

--- youtube_search/models.py
+++ youtube_search/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    date_published = models.DateTimeField()
+    thumbnail_url = models.TextField(null=True)
+    video_id = models.CharField(max_length=15 , unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('date_published',)
+
+class LatestDateTime(models.Model):
+    date_time = models.DateTimeField()
